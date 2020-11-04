6d48bfea60a1993d809f97f500221969a3e60b06
tge <tgeconf@gmail.com>
2020-11-04 14:16:40 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './assets/style/App.scss';
+
+function App() {
+    return (
+        <div className='wrapper'>
+            <div className='nav-container'></div>
+            <div className='main-container'>
+                <div className='tool-container'></div>
+                <div className='canvas-container'></div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/layout/canvas.tsx
@@ -0,0 +1,10 @@
+import '../../assets/style/canvas.scss';
+import React from 'react';
+
+export default class Canvas extends React.Component{
+    render(){
+        return (
+            <svg className='canvas-svg'></svg>
+        )
+    }
+}
