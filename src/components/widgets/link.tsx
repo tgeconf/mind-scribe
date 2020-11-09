7aad5901da802cd4ca99abbc0d659e8b8f4753be
tge <tgeconf@gmail.com>
2020-11-09 23:13:41 +0800

    update

--- src/components/widgets/link.tsx
+++ src/components/widgets/link.tsx
@@ -24,16 +24,24 @@ export default class Link extends React.Component<LinkProps, LinkState> {
     }
 
     screenToSvg(screenCoord: ICoord) {
-        
+        const svg: any = document.getElementById('svgContainer');
+        const svgOri: any = document.getElementById('svgOri');
+        const point = svg.createSVGPoint();
+        point.x = screenCoord.x;
+        point.y = screenCoord.y;
+        // point = coordinateTransform(point, svgChild);
+        const CTM = svgOri.getScreenCTM();
+        point.matrixTransform(CTM.inverse());
+        return point;
     }
 
     render() {
         return (
             <line
-                x1={this.props.startPnt.x}
-                y1={this.props.startPnt.y}
-                x2={this.props.endPnt.x}
-                y2={this.props.endPnt.y}
+                x1={this.screenToSvg(this.props.startPnt).x - 50}
+                y1={this.screenToSvg(this.props.startPnt).y - 40}
+                x2={this.screenToSvg(this.props.endPnt).x - 50}
+                y2={this.screenToSvg(this.props.endPnt).y - 40}
                 style={{ stroke: '#000', strokeWidth: 3 }}></line>
             // style = {{ stroke: '#f3f3f3f2', strokeWidth: 3 }}></line>
         )
